dca357980d37a4ab3edac8bea2c712acdcea94c5
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2023-01-20 13:58:07 +0900

    Add files via upload

--- ex08/integra.java
+++ ex08/integra.java
@@ -1,46 +1,59 @@
 public class integra {
     public static void main(String[] args){
-	    System.out.println("Numeral integration");
-	    System.out.println("Integral(1/(x^2 + 1)),  limit:[0,1]");
-	    
-	     new Trapezoidal().solve();
-	    new Simpson().solve();
-	    
+	System.out.println("Numeral integration");
+	System.out.println("Integral(1/(x^2 + 1)),  limit:[0,1]");
+	System.out.println();
+	new Trapezoidal().solve();
+	new Simpson().solve();
     }
 }
 
 class Trapezoidal{
     void solve(){
-	    int h=1-0;
+	//double h=1/24;
 	    int n=24;
+	    double h = 1.0 / (double)n;
 	    double result=0;
-	    for(int i=0;i<24;i++){
-		    result+=h/2*((1/(i*i+1))+(1/((i+1)*(i+1)+1)));
+	    double x=0;
+	    for(int i=0;i<n;i++){
+		result=result + ((1/(x*x+1)) + (1/((x+h)*(x+h)+1)));
+		x+=h;
 	    }
-	    
-	    System.out.println("I= " + result);
+
+	    System.out.println("Trapezoidal rule, nsub=24-----------");
+
+	    result=(double)(h)*result /2;
+	    System.out.println("I= " +result);
 	    double error=result-Math.PI/4;
-	    System.out.println("Error = " +error);
-	    double relative=error/(Math.PI/4);
-	    System.out.println("R= "+ relative);
+	    System.out.println("Error= " +error);
+	    double relative=error/(Math.PI/4)*100;
+	    System.out.println("R= "+ relative + "   %");
+	    System.out.println();
 
     }
 }
 
 class Simpson{
     void solve(){
-	    int h=1;
 	    int n=24;
+	    double h=1.0 / (double)n;
 	    double result=0;
-	    for(int i=0;i<=i-2;i=i+2){
-		    result=h/3 *((1/(i*i+1)) + ( 1 /((i+1)*(i+1)+1) ) + ( 1/ ((i+2)*(i+2)+1) ));
+	    double x=0;
+	    for(int i=0;i<=n-2;i=i+2){
+		    result=result + ((1/(x*x+1)) +
+					  ( 4* 1 /((x+h)*(x+h)+1) ) +
+					  ( 1/ ((x+2*h)*(x+2*h)+1) ));
+		    x=x + 2*h;
 	    }
+
+	    System.out.println("Simpson 1/3 rule, nsub=24--------");
+	    result=(double)h * result /3.0;
 	    
 	    System.out.println("I= "+result);
 	    double error=result-(Math.PI/4);
 	    System.out.println("Error= "+error);
-	    double relative=error/(Math.PI/4);
-	    System.out.println("R= "+relative);
+	    double relative=error/(Math.PI/4)*100;
+	    System.out.println("R= "+relative + "%");
 				 
 
     }
