b659af99600f52a65998605801e67ace951e8b8e
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2023-01-24 14:24:48 +0900

    Add files via upload

---
+++ ex10/ex10.java
@@ -0,0 +1,52 @@
+public class ex10 {
+    public static void main(String[] args){
+	System.out.println("d^2u/dx^2 + d^2u/dy^2 + 2 = 0, edge length=1");
+
+
+
+    }
+
+
+    void Liebman(double h){
+	int n=1/h;
+	double[][] x;
+	x=new double[n][];
+	for(int i=0;i<n;i++){
+	    x[i]=new double[n];
+	}
+
+	double[][] y;
+	y=new double[n][];
+	for(int i=0;i<n;i++){
+	    y[i]=new double[n];
+	}
+	
+
+
+
+
+
+    }
+
+
+    void SOR(double h){
+	int n=1/h;
+	double[][] x;
+	x=new double[n][];
+	for(int i=0;i<n;i++){
+	    x[i]=new double[n];
+	}
+
+	double[][] y;
+	y=new double[n][];
+	for(int i=0;i<n;i++){
+	    y[i]=new double[n];
+	}
+
+
+
+
+
+    }
+
+}
